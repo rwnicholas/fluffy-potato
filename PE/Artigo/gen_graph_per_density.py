eafca8cdab4f6056ff2004a65e550da30e0da960
Nicholas L. S. Silva <nicholas.silva@laccan.ufal.br>
2021-05-09 16:53:49 -0300

    Gerando grafos por n e densidade

---
+++ PE/Artigo/gen_graph_per_density.py
@@ -0,0 +1,38 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+class DenseXGraph:
+    G = None
+
+    def __init__(self, p, n, low_interv = 10, high_interv = 1000):
+        '''
+            p: Representa a densidade do gráfico em porcentagem
+
+            n: É o número de vértices
+
+            low_interv: Valor mínimo do intervalo do peso das arestas
+
+            high_interv: Valor máximo do intervalo do peso das arestas
+        '''
+
+        m = (10*(10-1)*p)/2
+        
+        self.G = nx.generators.random_graphs.dense_gnm_random_graph(n, np.rint(m))
+        while not nx.is_connected(self.G):
+            self.G = nx.generators.random_graphs.dense_gnm_random_graph(n, np.rint(m))
+        
+        for (u, v) in self.G.edges():
+            self.G.edges[u,v]['weight'] = np.random.randint(low_interv, high_interv)
+
+    def plot(self, figsize=(10, 15), label = True):
+        plt.figure(figsize=figsize)
+        
+        pos=nx.spring_layout(self.G)
+        nx.draw_networkx(self.G,pos)
+
+        if label == True:
+            labels = nx.get_edge_attributes(self.G,'weight')
+            nx.draw_networkx_edge_labels(self.G,pos,edge_labels=labels)
+        
+        plt.show()
---
+++ PE/Artigo/main.py
@@ -0,0 +1,7 @@
+import gen_graph_per_density as gpd
+import time
+
+start_time = time.time()
+graph = gpd.DenseXGraph(0.3, 10**2, 1, 100)
+print("--- %s seconds ---" % (time.time() - start_time))
+graph.plot()
