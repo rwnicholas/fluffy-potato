d99a86e24d551b983458854f03ba35d84739aab1
Nicholas L. S. Silva <nicholas.silva@laccan.ufal.br>
2021-05-14 16:52:30 -0300

    More graphs

--- PE/Artigo/main.py
+++ PE/Artigo/main.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 n_var = [250, 500, 1000, 10000]
 probab = [0.3, 0.6, 0.9, 1]
 
-for p in probab:
-    for n in n_var:
-        tmp_graph = gpd.DenseXGraph(p, n)
-        tmp_graph.export("graphs/graph_p_" + str(p) + "_n_" + str(n) + ".gexf")
+for i in range(1,11):
+    for p in probab:
+        for n in n_var:
+            tmp_graph = gpd.DenseXGraph(p, n)
+            tmp_graph.export("graphs/graph_i_" + str(i) + "_p_" + str(p) + "_n_" + str(n) + ".gexf")
