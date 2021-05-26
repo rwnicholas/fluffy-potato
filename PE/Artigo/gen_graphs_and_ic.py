f61e3644d49214f8c92ce03dc8010ad132f230e2
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2021-05-26 11:42:15 -0300

    Removing string from numerical data

--- PE/Artigo/gen_graphs_and_ic.py
+++ PE/Artigo/gen_graphs_and_ic.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+import pandas as pd
+import numpy as np
 
+# 'tempo' in seconds
+# 'memoria' in megabytes
+
+data = pd.read_csv("data/result.csv", dtype={
+    'ensaio': np.float64, 'densidade': np.float64,
+    'tempo': np.float64
+})
+
+print(data.head())
+# data.loc[data['algoritmo'] == "kruskal"].plot(x='ensaio', y='tempo')
+# print(data.loc[data['algoritmo'] == "kruskal"])
