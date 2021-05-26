57fb5c14efdbeae0eb94aab4673191d6c7247717
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2021-05-26 12:46:18 -0300

    testing

--- PE/Artigo/gen_graphs_and_ic.py
+++ PE/Artigo/gen_graphs_and_ic.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 'tempo' in seconds
 # 'memoria' in megabytes
 
 data = pd.read_csv("data/result.csv", dtype={
     'ensaio': np.float64, 'densidade': np.float64,
-    'tempo': np.float64
+    'tempo': np.float64, 'memoria': np.float64
 })
 
-print(data.head())
-# data.loc[data['algoritmo'] == "kruskal"].plot(x='ensaio', y='tempo')
-# print(data.loc[data['algoritmo'] == "kruskal"])
+# print(data.head())
+dataMean = data.groupby(['nvertices', 'densidade', 'algoritmo']).mean().drop(columns="ensaio")
+
+# dataMean.loc[dataMean['algoritmo'] == "kruskal"].plot.scatter(x='densidade', y='tempo')
+# plt.savefig('teste.png')
