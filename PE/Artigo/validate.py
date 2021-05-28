15f8e58a03385d0ea6670de252e3ac314f4594bb
Nicholas L S Silva <nicholas.silva@laccan.ufal.br>
2021-05-28 01:04:58 -0300

    Graficos finais

--- PE/Artigo/validate.py
+++ PE/Artigo/validate.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+from numpy.core.defchararray import title
 import pandas as pd
 from pandas.plotting import lag_plot
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+from sklearn.linear_model import LinearRegression
+from yellowbrick.regressor import ResidualsPlot
 
 # 'tempo' in seconds
 # 'memoria' in megabytes
@@ -18,13 +21,10 @@ dataBox = data.groupby(['algoritmo','densidade','nvertices']).mean().drop(column
 
 # dataBox.get_group('kruskal').plot.scatter(x='tempo', y='densidade')
 # # dataBox.plot.scatter(x='tempo', y='ensaio')
+data = data.drop(columns='ensaio')
 
-model = ols('memoria ~ nvertices + densidade + algoritmo', data=data).fit()
-print(model.summary())
-fig = plt.figure(figsize=(12,8))
+lnmodel = LinearRegression()
+vizualizer = ResidualsPlot(lnmodel, qqplot=True, hist=False, train_color='r', title="Residuais x Tempo")
+vizualizer.fit(data[['nvertices','densidade']], data['tempo'])
+vizualizer.show(outpath='residuais_tempo.png')
 
-obj = 'algoritmo[T.prim]'
-
-fig = sm.graphics.plot_regress_exog(model, obj,fig=fig)
-plt.savefig(obj+'_memoria.png')
-plt.close()
